d06f545d6d658502f3e99c8da9a1166e18259102
Admin <diegovaloriza@gmail.com>
2025-10-19 19:16:54 +0200

    Merge con Miriam casi terminado sin errores

--- app/src/main/java/com/diegodiaz/techwizards/app/MainActivity.kt
+++ app/src/main/java/com/diegodiaz/techwizards/app/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.diegodiaz.techwizards.app.ui.theme.TechWizardsTheme
+import com.diegodiaz.techwizards.ui.theme.TechWizardsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
